a01f44e891de1ddf4e0a1303cd6f8304073714fb
Steven Maloney <steven.maloney@gmail.com>
2021-02-24 15:28:31 -0600

    added facebook presentation

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -11,9 +11,12 @@ export default function About(){
                 I recently (February, 2021) finished a 20 week intensive, team project focused full 
                 stack web development program at <a href="https://primeacademy.io/">Prime Digital Academy</a>. 
             </p>
-            <p>I made a bunch of cool apps while at Prime 
-                and I am excited to go on making great things!
-            </p> 
+            <p>
+                Check out my teams presentation of our final project: The Result Foundation Grant Applicaiton Portal. I'm on at the 26:28 mark introducing our group project.
+            </p>
+            <p>
+                <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fprimedigitalacademy%2Fvideos%2F133700585281697%2F&show_text=false&width=560" width="560" height="314" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+            </p>
         </Container>
     )
 }
