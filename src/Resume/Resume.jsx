aa4304bdbd53fd5b6a19a8bf2b3a4e2a9aed6bf8
Steven Maloney <steven.maloney@gmail.com>
2021-02-28 14:12:57 -0600

    expanded resume section

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -23,6 +23,16 @@ export default function Resume() {
             ))}
             </GridList>
             </Grid>
+            <Grid container spacing={12} justify="center">
+                <h2>Biography</h2>
+                <p>When I was a political scientist, I researched 
+                    how systems of people learn. So, while it might not seem a normal transition, 
+                    I personally see moving into software engineering as a natural move into a different 
+                    field in information processing. I have always been interested in how information moves 
+                    through systems, and now I want to help build systems instead of just studying them.
+                </p>
+            </Grid>
+            
         </Container>
         
 
